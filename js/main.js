cc221b97746432f87ee0d8343c04b060dbeef381
osimuka <osimukaokpan@gmail.com>
2024-02-23 13:17:08 +0100

    some update

--- js/main.js
+++ js/main.js
@@ -7,3 +7,36 @@ document.addEventListener("DOMContentLoaded", () => {
     mobileView.classList.toggle("active");
   });
 });
+
+const element1 = document.querySelector(".link-list");
+const element2 = document.querySelector(".link-lists");
+document.addEventListener("DOMContentLoaded", () => {
+  element2.classList.add("current");
+});
+element1.addEventListener("click", () => {
+  element1.classList.add("current");
+  element2.classList.remove("current");
+});
+element2.addEventListener("click", () => {
+  element1.classList.remove("current");
+  element2.classList.add("current");
+});
+
+// const ele_link = document.querySelectorAll(".frame-links");
+// ele_link.forEach((each_link) => {
+//   each_link.addEventListener("click", () => {
+//     ele_link.forEach((p) => {
+//       p.classList.remove("active_click");
+//     });
+//     this.classList.add("active_click");
+//   });
+// });
+
+function changeColor(index) {
+  const ele_link = document.querySelectorAll(".frame-links");
+
+  ele_link.forEach((p) => {
+    p.classList.remove("active_click");
+  });
+  ele_link[index].classList.add("active_click");
+}
